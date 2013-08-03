787f23bebfb19a881057cf4413fba2f58a060ff2
Owen Synge <owen.synge@jaysnest.de>
2013-08-03 23:03:45 +0200

    initial import of 0.0.3

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='org-desy-grid-virt-sort-release',
+    version='0.0.3',
+    description="Sorts tags and similar so we always get the newest version",
+    author="O M Synge",
+    author_email="owen.Synge@desy.de",
+    url="www-it.desy.de",
+    scripts = ["org_desy_grid_virt_sort_release.py"],
+    
+)
